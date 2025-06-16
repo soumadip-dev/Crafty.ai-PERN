697bea6defb77d5b73cf14dd18bc24c530d034c8
soumadip-dev <soumadipmajila@gmail.com>
2025-06-16 13:00:00 +0530

    feat(routes): add routes for all controllers

--- server/src/routes/ai.routes.js
+++ server/src/routes/ai.routes.js
@@ -3,8 +3,12 @@ import {
   generateArticle,
   generateBlogTitle,
   generateImage,
+  removeImageBackground,
+  removeImageObject,
+  resumeReview,
 } from "../controllers/ai.controllers.js";
 import { auth } from "../middlewares/auth.js";
+import { upload } from "../configs/multer.js";
 
 // Creating router instance
 const router = Router();
@@ -17,5 +21,24 @@ router.post("/generate-blog-title", auth, generateBlogTitle);
 
 // Generate image route
 router.post("/generate-image", auth, generateImage);
+
+// Remove background from image route
+router.post(
+  "/remove-image-background",
+  upload.single("image"),
+  auth,
+  removeImageBackground,
+);
+
+// Remove any object from image route
+router.post(
+  "/remove-image-object",
+  upload.single("image"),
+  auth,
+  removeImageObject,
+);
+
+// Resume Review route
+router.post("/resume-review", upload.single("resume"), auth, resumeReview);
 
 export default router;
