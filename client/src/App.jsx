89d27e967dd9aaf670897c110968320c9bf7db8c
Soumadip Majila <soumadipmajila@gmail.com>
2025-07-07 12:00:00 +0530

    feat: set up routes using React Router DOM

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Layout from './pages/Layout.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import WriteArticle from './pages/WriteArticle.jsx';
+import BlogTitle from './pages/BlogTitle.jsx';
+import GenerateImages from './pages/GenerateImages.jsx';
+import RemoveBackground from './pages/RemoveBackground.jsx';
+import RemoveObject from './pages/RemoveObject.jsx';
+import RviewResume from './pages/ReviewResume.jsx';
+import Community from './pages/Community.jsx';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ai" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitle />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="review-resume" element={<RviewResume />} />
+          <Route path="community" element={<Community />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
